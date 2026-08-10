59d5dd4a8fc9cdb796b70e21a1fc5216e47fb9f5
Farbod Younesi <farbodyounesi@gmail.com>
2026-08-10 19:58:06 +0330

    Refactor main function to include heat variable

--- Project_1 (bang-bang)/main.py
+++ Project_1 (bang-bang)/main.py
@@ -2,14 +2,14 @@ from heat_simulation import Simulator
 
 def main() -> None:
     sim = Simulator()
-
+    heat = False
     try:
         while True:
             # write your code here
 
 
             
-            key = sim.run(True) #True for turning on the heating element, False for turning it off
+            key = sim.run(heat) #True for turning on the heating element, False for turning it off
             if key in (ord("q"), 27):  # 'q' or Esc to quit
                 break
     finally:
